e61cfd820850b86fbc6cf1b2306cc942f2fc42a9
Pavel Riedl <chochule@gmail.com>
2020-01-28 15:12:07 +0100

    Remove deprecated code.

--- MyCoordinateTransformation.cs
+++ MyCoordinateTransformation.cs
@@ -1796,67 +1796,6 @@ namespace IDEA.Library
             return new WGS84Coordinate(Fl1Rad * 57.295779513082323d + dFIsec, LaRad * 57.295779513082323d + dLAsec);
         }
 
-        /* Tato funkce pracuje správně jen pro zónu N a 'nějak' nepočítá s dalšími. Dále obsahuje
-           také části a proměnné, které jsou počítány, ale nejsou nikde využity. Mám pocit, že funkce není
-           dokončená a tak jsem ji radši deaktivoval.
-           
-        public static WGS84Coordinate TransformWGS84(UTMCoordinate utmCoordinate)
-        {
-            var elipsoid = Elipsoid.GetDefault();
-            var eccSquared = elipsoid.EccSquared;
-            var a = elipsoid.A;
-
-            double UTMEasting = utmCoordinate.UTMEasting;
-            double UTMNorthing = utmCoordinate.UTMNorthing;
-            int UTMZoneNumber = utmCoordinate.ZoneNumber;
-            string UTMZoneLetter = utmCoordinate.ZoneLetter;
-
-            var e1 = (1 - Math.Sqrt(1 - eccSquared)) / (1 + Math.Sqrt(1 - eccSquared));
-            var x = UTMEasting - 500000.0; //remove 500,000 meter offset for longitude
-            var y = UTMNorthing;
-            var ZoneNumber = UTMZoneNumber;
-            var ZoneLetter = UTMZoneLetter;
-            int NorthernHemisphere;
-
-            if ("N" == ZoneLetter)
-            {
-                NorthernHemisphere = 1;
-            }
-            else
-            {
-                NorthernHemisphere = 0;
-                y -= 10000000.0;
-            }
-
-            var LongOrigin = (ZoneNumber - 1) * 6 - 180 + 3;
-
-            var eccPrimeSquared = eccSquared / (1 - eccSquared);
-
-            double M = y / 0.9996;
-            var mu = M / (a * (1 - eccSquared / 4 - 3 * eccSquared * eccSquared / 64 - 5 * eccSquared * eccSquared * eccSquared / 256));
-
-            var phi1Rad = mu + (3 * e1 / 2 - 27 * e1 * e1 * e1 / 32) * Math.Sin(2 * mu)
-                    + (21 * e1 * e1 / 16 - 55 * e1 * e1 * e1 * e1 / 32) * Math.Sin(4 * mu)
-                    + (151 * e1 * e1 * e1 / 96) * Math.Sin(6 * mu);
-            var phi1 = phi1Rad * RAD_TO_DEG;
-
-            var N1 = a / Math.Sqrt(1 - eccSquared * Math.Sin(phi1Rad) * Math.Sin(phi1Rad));
-            var T1 = Math.Tan(phi1Rad) * Math.Tan(phi1Rad);
-            var C1 = eccPrimeSquared * Math.Cos(phi1Rad) * Math.Cos(phi1Rad);
-            var R1 = a * (1 - eccSquared) / Math.Pow(1 - eccSquared * Math.Sin(phi1Rad) * Math.Sin(phi1Rad), 1.5);
-            var D = x / (N1 * 0.9996);
-
-            var Lat = phi1Rad - (N1 * Math.Tan(phi1Rad) / R1) * (D * D / 2 - (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * eccPrimeSquared) * D * D * D * D / 24
-                    + (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * eccPrimeSquared - 3 * C1 * C1) * D * D * D * D * D * D / 720);
-            Lat = Lat * RAD_TO_DEG;
-
-            var Long = (D - (1 + 2 * T1 + C1) * D * D * D / 6 + (5 - 2 * C1 + 28 * T1 - 3 * C1 * C1 + 8 * eccPrimeSquared + 24 * T1 * T1)
-                    * D * D * D * D * D / 120) / Math.Cos(phi1Rad);
-            Long = LongOrigin + (Long * RAD_TO_DEG);
-            
-            return new WGS84Coordinate(Lat, Long);
-        }
-        */
 
         /// <summary>
         /// Transformuje souřadnice WGS84 na UTM
